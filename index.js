dfd7d8f086c32fc17a4bc926261052aaa005be73
gopesh3652 <gopeshkhandelwal10@gmail.com>
2024-04-29 10:51:09 +0530

    Invalid city error handled

--- index.js
+++ index.js
@@ -8,23 +8,32 @@ const weatherIcon = document.querySelector(".weather-image");
 
 async function checkWeather(city) {
   const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
-  const data = await response.json();
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML =
-    Math.round(data.main.temp) + " °c";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+  if (response.status === 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error").style.display = "block";
+  } else {
+    const data = await response.json();
 
-  const weatherCondition = data.weather[0].main;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + " °c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
-  if (weatherCondition == "Clouds") weatherIcon.src = "images/clouds.png";
-  else if (weatherCondition == "Rain") weatherIcon.src = "images/rain.png";
-  else if (weatherCondition == "Snow") weatherIcon.src = "images/snow.png";
-  else if (weatherCondition == "Clear") weatherIcon.src = "images/clear.png";
-  else if (weatherCondition == "Drizzle")
-    weatherIcon.src = "images/drizzle.png";
-  else weatherIcon.src == "images/mist.png";
+    const weatherCondition = data.weather[0].main;
+
+    if (weatherCondition == "Clouds") weatherIcon.src = "images/clouds.png";
+    else if (weatherCondition == "Rain") weatherIcon.src = "images/rain.png";
+    else if (weatherCondition == "Snow") weatherIcon.src = "images/snow.png";
+    else if (weatherCondition == "Clear") weatherIcon.src = "images/clear.png";
+    else if (weatherCondition == "Drizzle")
+      weatherIcon.src = "images/drizzle.png";
+    else weatherIcon.src == "images/mist.png";
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
